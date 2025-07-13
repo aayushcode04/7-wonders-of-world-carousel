0e05c3599ec6ca57d3ea5a4f1039e1454638bc5b
aayushcode04 <aayushagrawal184@gmail.com>
2025-07-13 23:32:59 +0530

    stopping fast running

--- slider.js
+++ slider.js
@@ -4,25 +4,33 @@ let sliderDom = document.querySelector('.slider');
 let listItemDom = document.querySelector('.slider .list');
 let thumbnailDom = document.querySelector('.slider .thumbnail');
 
-nextDom.onclick = function(){
-    showSlider('next');
-}
-
-prevDom.onclick = function(){
-    showSlider('prev');
-}
-
-// Adding keyboard functionality for left and right arrows
-document.addEventListener('keydown', function(event) {
-    if(event.key === 'ArrowRight') {
-        showSlider('next');
-    } else if(event.key === 'ArrowLeft') {
-        showSlider('prev');
-    }
-});
+// Track if an animation is in progress
+let isAnimating = false;
 
 let timeRunning = 3000;
 let runTimeOut;
+
+// Disable clicks while animating
+nextDom.onclick = () => handleSlide('next');
+prevDom.onclick = () => handleSlide('prev');
+
+// Keyboard navigation
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowRight') {
+        handleSlide('next');
+    } else if (event.key === 'ArrowLeft') {
+        handleSlide('prev');
+    }
+});
+
+function handleSlide(type) {
+    if (isAnimating) {
+        alert('Please wait for the slide to finish.');
+        return;
+    }
+    isAnimating = true;
+    showSlider(type);
+}
 
 function showSlider(type){
     let itemSlider = document.querySelectorAll('.slider .list .item');
@@ -32,16 +40,18 @@ function showSlider(type){
         listItemDom.appendChild(itemSlider[0]);
         thumbnailDom.appendChild(itemThumbnail[0]);
         sliderDom.classList.add('next');
-    }else{
-        let positionLastItem = itemSlider.length - 1 ;
+    } else {
+        let positionLastItem = itemSlider.length - 1;
         listItemDom.prepend(itemSlider[positionLastItem]);
         thumbnailDom.prepend(itemThumbnail[positionLastItem]);
         sliderDom.classList.add('prev');
     }
 
     clearTimeout(runTimeOut);
+
     runTimeOut = setTimeout(() => {
         sliderDom.classList.remove('next');
         sliderDom.classList.remove('prev');
-    }, timeRunning)
+        isAnimating = false; // Re-enable clicks
+    }, timeRunning);
 }
